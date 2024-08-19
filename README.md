fb606569eb025aea4e1dd227cbcf276ac227fcf0
yalhag1 <yalhag@gmail.com>
2024-08-19 20:49:01 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## train_ms
+
+train_ms
+
+#### License
+
+MIT
---
+++ train_ms/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "train_ms",
+			"type": "module",
+			"label": _("train_ms")
+		}
+	]
